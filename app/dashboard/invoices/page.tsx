1256f5b133cd38e150fbc1e3ea176ca67138de51
Long Nguyen <longnghoang@outlook.com>
2026-04-27 15:33:34 +0700

    feat: Chapter 15 - Adding Metadata

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -6,13 +6,20 @@ import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
- 
-export default async function Page(props: { searchParams?: Promise<{ query?: string; page?: string }> }) {
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Invoices',
+};
+
+export default async function Page(props: {
+  searchParams?: Promise<{ query?: string; page?: string }>;
+}) {
   const searchParams = await props.searchParams;
   // console.log('searchParams :', searchParams, 'Current date: ', new Date());
   const query = searchParams?.query || '';
   const currentPage = Number(searchParams?.page) || 1;
-  const totalPages = await fetchInvoicesPages(query);  
+  const totalPages = await fetchInvoicesPages(query);
 
   return (
     <div className="w-full">
@@ -23,7 +30,7 @@ export default async function Page(props: { searchParams?: Promise<{ query?: str
         <Search placeholder="Search invoices..." />
         <CreateInvoice />
       </div>
-       <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
         <Table query={query} currentPage={currentPage} />
       </Suspense>
       <div className="mt-5 flex w-full justify-center">
